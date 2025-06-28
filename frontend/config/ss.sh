1d7a38abe03656dc236cc4fbc4a5877df7fa697d
EthanWickenkamp <102557780+EthanWickenkamp@users.noreply.github.com>
2025-06-28 15:51:14 -0500

    dependencies not dev
    as

--- frontend/config/ss.sh
+++ frontend/config/ss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e #exit on error
+set -e # exit on error
 
 cd /app/myapp
 
@@ -9,10 +9,13 @@ echo "Cleaning up old build..."
 rm -rf node_modules
 rm -rf .svelte-kit
 rm -rf build
-#rm -f package-lock.json
 
 echo "Installing dependencies..."
 npm install
+
+# Run svelte-kit sync to generate .svelte-kit directory
+echo "Syncing SvelteKit..."
+npm run prepare
 
 echo "Building for production..."
 npm run build
