4300f101f24a9beb409dfb25e14250f4a474179f
EthanWickenkamp <102557780+EthanWickenkamp@users.noreply.github.com>
2025-06-29 09:48:24 -0500

    let datta

--- frontend/myapp/src/routes/+page.svelte
+++ frontend/myapp/src/routes/+page.svelte
@@ -2,5 +2,5 @@
 	export let data;
 </script>
 
-<h1>Welcome {data.user.username ?? 'to Svango template'}</h1>
+<h1>Welcome {data.user?.username ?? 'to Svango template'}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
