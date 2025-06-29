bef1de2206dc956843987d1466f97a69af887e62
EthanWickenkamp <102557780+EthanWickenkamp@users.noreply.github.com>
2025-06-29 09:30:30 -0500

    no more store

--- frontend/myapp/src/routes/+page.svelte
+++ frontend/myapp/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { user } from '$lib/stores/user';
 </script>
 
-<h1>Welcome {$user?.username ?? 'to Svango template'}</h1>
+<h1>Welcome {data.user.username ?? 'to Svango template'}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
